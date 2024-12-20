c8b3e4a901c70e3d4e062cce98bca16ef3664afc
ysalama03 <yusufsalama03@gmail.com>
2024-12-20 21:38:23 +0200

    removed generic in IM

--- Fetch/Instruction_memory.vhd
+++ Fetch/Instruction_memory.vhd
@@ -3,23 +3,20 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Instruction_memory is 
-generic( 
-	address_bits : integer := 12; 
-	word_width : integer := 16 ); 
 port(
 	--inputs 
-	address 	: in std_logic_vector(address_bits-1 downto 0);
+	address 	: in std_logic_vector(12 downto 0);
 	--outputs
-	instruction : out std_logic_vector(word_width-1 downto 0); 
-	immediate 	: out std_logic_vector(word_width-1 downto 0);
-	IM_3 		: out std_logic_vector(word_width-1 downto 0)
+	instruction : out std_logic_vector(15 downto 0); 
+	immediate 	: out std_logic_vector(15 downto 0);
+	IM_3 		: out std_logic_vector(15 downto 0)
 	); 
 end entity;
 
 
 ARCHITECTURE Instruction_memory_arch OF Instruction_memory IS
 
-TYPE Rom_type IS ARRAY(0 TO 2**address_bits) OF STD_LOGIC_VECTOR(word_width-1 DOWNTO 0);
+TYPE Rom_type IS ARRAY(0 TO 2**12) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
 SIGNAL memory: Rom_type := (
 	others => (others => '0') );
 BEGIN 
