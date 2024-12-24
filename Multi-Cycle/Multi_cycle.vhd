02e2aa3351d5d284a1ccfa8d42ff4587c728ed38
ysalama03 <yusufsalama03@gmail.com>
2024-12-25 01:15:55 +0200

    debuged loose signals

--- Multi-Cycle/Multi_cycle.vhd
+++ Multi-Cycle/Multi_cycle.vhd
@@ -216,7 +216,7 @@ signal pc_outf: std_logic_vector(15 downto 0); -- from fetch to fd reg
 signal instruction: std_logic_vector(15 downto 0); -- from Fetch to FD reg
 signal immediate: std_logic_vector(15 downto 0); -- from Fetch to FD reg
 --signal IM_3: std_logic_vector(15 downto 0); -- from Fetch to FD reg
-signal stall : std_logic; -- from Hazard Detection to Fetch
+signal stall : std_logic := '0'; -- from Hazard Detection to Fetch
 
 signal opcode: std_logic_vector(4 downto 0); -- from FD reg to Decode
 signal RA1: std_logic_vector(2 downto 0); -- from FD reg to Decode
@@ -224,7 +224,7 @@ signal RA2: std_logic_vector(2 downto 0); -- from FD reg to Decode
 signal WA: std_logic_vector(2 downto 0); -- from FD reg to Decode
 signal pc_outfd: std_logic_vector(15 downto 0); -- from FD reg to Decode
 signal ctrl_bus_d: std_logic_vector(23 downto 0); -- from Decode to DE reg
-signal flush_br: std_logic; -- from Decode to DE reg
+signal flush_br: std_logic := '0'; -- from Decode to DE reg
 signal RD1: std_logic_vector(15 downto 0); -- from Decode to DE reg
 signal RD2: std_logic_vector(15 downto 0); -- from Decode to DE reg
 
@@ -238,8 +238,8 @@ signal WA_e: std_logic_vector(2 downto 0); -- from DE reg to Execute
 signal pc_outde: std_logic_vector(15 downto 0); -- from DE reg to Execute
 
 signal exception: std_logic; -- from Fetch to Hazard Detection
-signal flush_ex: std_logic; -- from Hazard Detection to DE reg
-signal flush: std_logic; -- flush_br or flush_ex
+signal flush_ex: std_logic:= '0'; -- from Hazard Detection to DE reg
+signal flush: std_logic:= '0'; -- flush_br or flush_ex
 
 signal mem_to_alu: std_logic_vector(15 downto 0); -- from Memory to Execute
 signal alu_to_alu: std_logic_vector(15 downto 0); -- from Execute to Execute
@@ -260,7 +260,6 @@ signal SP_address: std_logic_vector(15 downto 0); -- from Memory to MWB reg
 signal Memory_address: std_logic_vector(15 downto 0); -- from Memory to MWB reg
 signal From_Memory: std_logic_vector(15 downto 0); -- from Memory to MWB reg
 signal restore_flags: std_logic_vector(2 downto 0); -- from Memory to Execute
-signal ctrl_bus_m: std_logic_vector(23 downto 0); -- from Memory to MWB reg
 signal WA_m: std_logic_vector(2 downto 0); -- from Memory to MWB reg
 
 signal WA_mw: std_logic_vector(2 downto 0); -- from MW reg to WB
@@ -282,10 +281,10 @@ DE: DE_reg PORT MAP(clk, flush, stall, ctrl_bus_d, RD1, RD2, immediate, RA1, RA2
 exception <= SP_ex or invld_mem;
 HD: Hazard_Detection PORT MAP(exception, RA1, RA2, WA_e, stall, flush_ex);
 flush <= flush_br or flush_ex;
-E: Execute PORT MAP(clk, ctrl_bus_e, RD1_e, mem_to_alu, alu_to_alu, RD2_e, immediate_e, inport, restore_flags, OP1_Selector, OP2_Selector, pc_outde, ctrl_bus_m(0), pc_oute, alu_out, pc_src_e);
+E: Execute PORT MAP(clk, ctrl_bus_e, RD1_e, mem_to_alu, alu_to_alu, RD2_e, immediate_e, inport, restore_flags, OP1_Selector, OP2_Selector, pc_outde, ctrl_bus_em(0), pc_oute, alu_out, pc_src_e);
 EM: EM_reg PORT MAP(clk, flush, stall, ctrl_bus_e, RD2_e, WA_e, alu_out, pc_oute, pc_src_e, ctrl_bus_em, RD2_em, WA_em, alu_out_em, pc_outem, pc_src_em);
 M: Memory PORT MAP(clk, ctrl_bus_em, alu_out_em, RD2_em, pc_outem, SP_address, Memory_address, From_Memory, restore_flags);
-MWB: MWB_reg PORT MAP(clk, stall, pc_src_em, ctrl_bus_m, From_Memory, alu_out_em, WA_m, ctrl_bus_mw, From_Memory_out, From_ALU_out, WA_mw, pc_src_mw);
+MWB: MWB_reg PORT MAP(clk, stall, pc_src_em, ctrl_bus_em, From_Memory, alu_out_em, WA_em, ctrl_bus_mw, From_Memory_out, From_ALU_out, WA_mw, pc_src_mw);
 FU: Forwarding_Unit PORT MAP(RA1_e, RA2_e, WA_em, WA_mw, ctrl_bus_em(23), ctrl_bus_mw(23), OP1_Selector, OP2_Selector);
 EH: Exception_Handler PORT MAP(clk, ctrl_bus_em(5), ctrl_bus_em(4), ctrl_bus_em(21)  , ctrl_bus_em(22)  ,  SP_address, Memory_address, Save_EPC, SP_ex, invld_mem);
 e_pc: EPC PORT MAP(clk, Save_EPC, pc_outem, EPC_out);
