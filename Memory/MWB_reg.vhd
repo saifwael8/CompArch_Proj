e731c6fbb55a5249631a62c951a93814249542f1
youssefehab212 <youssefehab534@gmail.com>
2024-12-26 01:47:50 +0200

    removed stall

--- Memory/MWB_reg.vhd
+++ Memory/MWB_reg.vhd
@@ -4,7 +4,6 @@ use ieee.numeric_std.all;
 
 entity MWB_reg is
 port( clk: in std_logic;
-		stall: in std_logic;
 		PCSrc: in std_logic;
 		
 		Control_Bus: in std_logic_vector(23 downto 0);
@@ -29,13 +28,11 @@ PROCESS(clk)
 BEGIN
 
 	IF rising_edge(clk) then
-		IF (stall = '0') then
 			Control_Bus_out <= Control_Bus;
 			From_Memory_out <= From_Memory;
 			From_ALU_out <= From_ALU;
 			WA_out <= WA;
 			PCSrc_out <= PCSrc;
-		END IF;
 	END IF;
 	
 END PROCESS;
