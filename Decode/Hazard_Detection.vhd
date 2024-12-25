e4a9d58450f5072c1b0793aac63e6a595e285579
youssefehab212 <youssefehab534@gmail.com>
2024-12-26 01:43:27 +0200

    needs to change stall back to 0

--- Decode/Hazard_Detection.vhd
+++ Decode/Hazard_Detection.vhd
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 entity Hazard_Detection is 
 port(
+	 clk: in std_logic;
     exception: in std_logic;
     FD_RA1: in std_logic_vector(2 downto 0);
     FD_RA2: in std_logic_vector(2 downto 0);
@@ -16,21 +17,30 @@ port(
 end entity;
 
 architecture HD_arch of Hazard_Detection is 
-
+signal curr_stall : std_logic := '0';
 begin
 
 Flush_exc <= exception; --flush if an exception occurs
 
 --- Load Use Hazard Detection ------
-PROCESS (FD_RA1,FD_RA2,DE_WA,DE_MR)
+PROCESS (clk,FD_RA1,FD_RA2,DE_WA,DE_MR)
 begin
-
-    IF ( (FD_RA1=DE_WA OR FD_RA2=DE_WA) AND DE_MR = '1' ) then
-        stall <= '1';
-    else
-        stall <= '0';
-    end if;
-
+	 
+	 
+		 IF ( (FD_RA1=DE_WA OR FD_RA2=DE_WA) AND DE_MR = '1' ) then
+        curr_stall <= '1';
+		 else
+			  curr_stall <= '0';
+		 end if;
+		 
+		 if (curr_stall = '1' and rising_edge(clk)) then
+			curr_stall <= '0';
+		 end if;
+	 
+	 
 end PROCESS;
+
+stall <= curr_stall;
+
 
 end architecture;
